e622f26af4ffd5fce99de81afa787e470fb6509f
19_web_cyr <19_web_cyr@DESKTOP-R0OB2SQ>
2023-11-13 21:42:37 +0900

    board practice1

--- 13_jsp_mvc1_prac1/WebContent/step01_boardEx1/bList1.jsp
+++ 13_jsp_mvc1_prac1/WebContent/step01_boardEx1/bList1.jsp
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<%@page import="step01_boardEx1.BoardDTO1"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="step01_boardEx1.BoardDAO1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
 <head>
 <meta charset="UTF-8">
@@ -16,15 +20,29 @@
 				<td>작성일</td>
 				<td>조회수</td>
 			</tr>
-			<%
+<%-- 			<%
 				int idx = 1;
-				ArrayList<BoardDTO>
+				ArrayList<BoardDTO1> boardList = BoardDAO1.getInstance().getBoardList();
+				for (BoardDTO1 boardDTO : boardList) {			
 			%>
-					<tr>
+ 					<tr>
 						<td><%=idx %></td>
+						<td><a href="bDetail.jsp?boardId=<%=BoardDTO1.getBoardId()%>"><%=boardDTO1.getSubject() %></a></td>
+						<td><%=BoardDTO1.getWriter() %></td>
+						<td><%=boardDTO1.getEnrollDt() %></td>
+						<td><%=boardDTO1.getReadCnt() %></td>
 					</tr>
 			
+			<%
+					idx++;
+				}
+			%> --%>
 			
+			<tr>
+				<td colspan="5">
+					<input type="button" value="글쓰기" onclick="location.href='bWrite.jsp'">
+				</td>
+			</tr>
 		</table>
 	</div>
 
--- 13_jsp_mvc1_prac1/src/step01_boardEx1/BoardDAO1.java
+++ 13_jsp_mvc1_prac1/src/step01_boardEx1/BoardDAO1.java
@@ -1,5 +1,106 @@
 package step01_boardEx1;
 
-public class BoardDAO1 {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+public class BoardDAO1 {
+	
+	// Singleton
+	private BoardDAO1() {}
+	private static BoardDAO1 instance = new BoardDAO1();
+	public static BoardDAO1 getInstance() {
+		return instance;
+	}
+	
+	private Connection conn         = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs            = null;
+	
+	// 데이터 베이스 연결 메서드
+	private void getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MVC1_PRACTICE?serverTimezone=Asia/Seoul", "root", "1234");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 데이터베이스 연결 해제 메서드
+	private void getClose() {
+		if (rs != null) try {rs.close();} catch (SQLException e1) {e1.printStackTrace();}
+		if (pstmt != null) try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+		if (conn != null) try {conn.close();} catch (SQLException e) {e.printStackTrace();}
+	}
+	
+	// 게시글 추가 DAO
+	public void insertBoard(BoardDTO1 boardDTO) {
+		
+		// 단위 테스트
+		System.out.println(boardDTO);
+		
+			   
+		try {
+			getConnection();
+			
+			String sql = "INSERT INTO BOARD(WRITER, EMAIL, SUBJECT, PASSWORD, CONTENT, READ_CNT, ENROLL_DT)";
+			sql += "VALUES(?,?,?,?,?,0,NOW())";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardDTO.getWriter());
+			pstmt.setString(2, boardDTO.getEmail());
+			pstmt.setString(3, boardDTO.getSubject());
+			pstmt.setString(4, boardDTO.getPassword());
+			pstmt.setString(5, boardDTO.getContent());
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			getClose();
+		}
+	}
+	
+	// 게시글 전체 조회 DAO
+	public ArrayList<BoardDTO1> getBoardList() {
+		
+		ArrayList<BoardDTO1> boardList = new ArrayList<BoardDTO1>();
+		
+		getConnection();
+		
+		try {
+			pstmt = conn.prepareStatement("SELECT * FROM BOARD");
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				
+				BoardDTO1 temp = new BoardDTO1();
+				temp.setBoardId(boardId); // 여기 하다가 맘!!!!!!
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return boardList;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
